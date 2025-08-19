1a21d58c8e342d33f23635759ecfc6412263c6e0
kartikbtspl <kartik@brilliantechsoft.com>
2025-08-19 12:34:10 +0530

    fixed the type err

--- components/common/ui/Input.tsx
+++ components/common/ui/Input.tsx
@@ -2,7 +2,7 @@
 "use client";
 import React, { forwardRef } from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
   label?: string;
   error?: string;
   helperText?: string;
