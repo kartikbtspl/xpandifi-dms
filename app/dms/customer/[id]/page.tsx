cd9fa4f0ad4a3510327140de9686bbda6f5e3856
ajinkya-darkase <ajinkya@brilliantechsoft.com>
2025-08-22 12:53:08 +0530

    conflicts resolved

--- app/dms/customer/[id]/page.tsx
+++ app/dms/customer/[id]/page.tsx
@@ -9,11 +9,11 @@ import { title } from "process";
 import StickyNotesData from "./components/Sticky";
 import {FaStore, FaTag, FaUserCheck, FaEnvelope, FaPhone, FaMapMarkerAlt} from "react-icons/fa";
 import BarChartCard from "./components/BarChartCard";
-import ToggleComp from "./components/ToggleComp";
 import Cards from "./components/Cards";
 import {FaChartLine,FaShoppingCart,FaFileInvoice,FaExclamationTriangle,FaMoneyCheckAlt,} from "react-icons/fa";
 import Table from "../../../../components/common/table/Table";
 import getStockColumns from "./components/getStockDetails"
+import ToggleComp from "./components/ToggleComp";
 // import other detail components (SalesOrdersChart, InfoCards, StockTable, etc.)
 
 export default function CustomerDetail() {
